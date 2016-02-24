b63ec88f0514406045c33161e1ea8227fd40c786
carlcs <seelbach@gmx.de>
2016-02-24 15:14:33 +0100

    Added French language

---
+++ translations/fr.php
@@ -0,0 +1,14 @@
+<?php
+
+return array(
+    'Maintenance' => 'Maintenance',
+    'Maintenance is complete.' => 'La maintenance est terminé.',
+    'Maintenance in progress.' => 'La maintenance s’effectue.',
+    'Maintenance will be carried out on {date}.' => 'La maintenance commence du {date}.',
+    'Maintenance will be carried out in {minutes} minutes.' => 'La maintenance commence dans {minutes} minutes.',
+    'Announcements' => 'Annonces',
+    'Announcement' => 'Annonce',
+    'No announcements are available.' => 'Aucune annonce n’est disponible.',
+    'Scheduled for {date}' => 'Prévue pour {date}',
+    'started {date}' => 'a commencé à {date}',
+);
---
+++ translations/xx.php
@@ -0,0 +1,43 @@
+<?php
+
+return array(
+    // User
+    'Maintenance' => 'xx',
+    'Maintenance is complete.' => 'xx',
+    'Maintenance in progress.' => 'xx',
+    'Maintenance will be carried out on {date}.' => 'xx',
+    'Maintenance will be carried out in {minutes} minutes.' => 'xx',
+    'Announcements' => 'xx',
+    'Announcement' => 'xx',
+    'No announcements are available.' => 'xx',
+    'Scheduled for {date}' => 'xx',
+    'started {date}' => 'xx',
+
+    // Admin and settings
+    'New Announcement' => 'xx',
+    'Create a new announcement' => 'xx',
+    'Edit announcement' => 'xx',
+    'Announcement saved.' => 'xx',
+    'Couldn’t save announcement.' => 'xx',
+    'No announcements exist yet.' => 'xx',
+    'Backend Maintenance' => 'xx',
+    'Frontend Maintenance' => 'xx',
+    'Start date is greater than end date.' => 'xx',
+    'Start Date' => 'xx',
+    'End Date' => 'xx',
+    'Pending' => 'xx',
+    'In progress' => 'xx',
+    'Completed' => 'xx',
+    'Whitelist URLs' => 'xx',
+    'Whitelist IP Addresses' => 'xx',
+    'Status “Pending”' => 'xx',
+    'Status “Imminent”' => 'xx',
+    'The settings are loaded from the {path} config file.' => 'xx',
+    'Define the URLs that are accessible when frontend maintenance is in progress.' => 'xx',
+    'Define the list of IP addresses that have access to the site when frontend maintenance is in progress.' => 'xx',
+    'The time in advance notification banners are shown.' => 'xx',
+    'The time in advance a countdown is shown in the notification banner instead of the start date.' => 'xx',
+    'Access the site when frontend maintenance is in progress' => 'xx',
+    'Close the “Maintenance in progress” overlay' => 'xx',
+    'Doesn’t get the “Maintenance in progress” overlay at all' => 'xx',
+);
