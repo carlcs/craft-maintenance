bb6f70457bf04269e1ae2d8120002d0698090c8b
carlcs <seelbach@gmx.de>
2016-02-25 02:04:49 +0100

    Prepare 1.1.0 release

--- MaintenancePlugin.php
+++ MaintenancePlugin.php
@@ -13,7 +13,7 @@ class MaintenancePlugin extends BasePlugin
 
     public function getVersion()
     {
-        return '1.0.2';
+        return '1.1.0';
     }
 
     public function getSchemaVersion()
