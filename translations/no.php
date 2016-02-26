80f1ee1b5a4e97d7775f1ffa4baa480c1b0f1d58
Espen Solli Grande <espen@mustasj.no>
2016-02-26 16:17:31 +0100

    Create no.php

---
+++ translations/no.php
@@ -0,0 +1,35 @@
+<?php
+
+return array(
+    'Maintenance' => 'Vedlikehold',
+    'Maintenance is complete.' => 'Vedlikehold er ferdig.',
+    'Maintenance in progress.' => 'Vedlikehold pågår.',
+    'Maintenance will be carried out on {date}.' => 'Vedlikehold vil pågå den {date}.',
+    'Maintenance will be carried out in {minutes} minutes.' => 'Vedlikehold starter om {minutes} minutter.',
+    'Announcements' => 'Annonseringer',
+    'Announcement' => 'Annonsering',
+    'No announcements are available.' => 'Ingen annonseringer er tilgjengelige.',
+    'Scheduled for {date}' => 'Planlagt {date}',
+    'started {date}' => 'startet {date}',
+
+	// Widget
+    'Maintenance Status' => 'Vedlikeholdstatus',
+    'Messages without a maintenance announcement' => 'Meldinger uten vedlikeholdsannonseringer',
+    'Pending' => 'Venter',
+    'In progress' => 'Pågår',
+    'Completed' => 'Ferdig',
+
+	// Admin and settings
+    'New Announcement' => 'Ny annonsering',
+    'Create a new announcement' => 'Lag ny annonsering',
+    'Edit announcement' => 'Rediger annonsering',
+    'Announcement saved.' => 'Annonsering lagret.',
+    'Couldn’t save announcement.' => 'Kunne ikke lagre annonsering.',
+    'No announcements exist yet.' => 'Ingen annonseringer eksisterer enda.',
+    'Backend Maintenance' => 'Backend vedlikehold',
+    'Frontend Maintenance' => 'Frontend vedlikehold',
+    'End Date is less than Start Date.' => 'Slutt tid er mindre enn start tid.',
+    'Start Date cannot be blank.' => 'Start tid kan ikke være tom.',
+    'Start Date' => 'Start tid',
+    'End Date' => 'Slutt tid',
+);
